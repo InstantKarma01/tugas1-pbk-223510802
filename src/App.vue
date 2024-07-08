368abbf7c25fd67693a4f4ed0e8b938b8a5a20fb
InstantKarma01 <tmsirhansamiralhinduan@gmail.com>
2024-07-08 23:24:00 +0700

    V1.0

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div id="loc">
+    <img alt="CV Image" class="logo" src="@/assets/cv.jpg" width="600" height="650" />
+  </div>
+</template>
+
+<style scoped>
+.loc {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+</style>
